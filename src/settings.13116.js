390f95f6e29863193b4f7b269642464a5b01238a
chommab <136581572+chommab@users.noreply.github.com>
2023-08-26 16:14:20 +0800

    Add files via upload

---
+++ src/settings.13116.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Loading.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"69b9a",main:"86f41"}};
